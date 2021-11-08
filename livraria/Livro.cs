d2f3431b895e6734ad57a75c30ad940d4d027084
francisco <franads@gmail.com>
2021-11-08 15:03:06 -0300

    trabalho

---
+++ livraria/Livro.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+using System.Collections.Generic;
+using System.Text;
+
+namespace livraria
+{
+    class Livro
+    {
+        public string cod; 
+        public string nome;
+        public double valor;
+
+
+
+
+        
+
+
+
+    }
+}
---
+++ livraria/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace livraria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro livro = new Livro();
+           Console.WriteLine("Digite o código do Livro");
+            livro.cod = Console.ReadLine();
+
+
+            Console.WriteLine("Digite o nome do Livro");
+            livro.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o valor do Livro");
+            livro.valor = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Código do Livro  " + livro.cod);
+            Console.WriteLine("nome do Livro  "+ livro.nome);
+            Console.WriteLine($"valor do Livro  {livro.valor.ToString("C")}"); 
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
